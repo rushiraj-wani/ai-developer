0011d0e705bac0b37b6676665693a731e010321c
Rushiraj Wani <rajwani2003@gmail.com>
2024-12-31 10:01:46 +0530

    added user login and middleware

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 import morgan from "morgan";
 import connect from "./db/db.js";
 import userRoutes from "./routes/user.routes.js";
+import cookieParser from "cookie-parser";
 
 connect();
 
@@ -11,6 +12,8 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/users", userRoutes);
 
